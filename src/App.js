216f7bbe62e216b32533dcda3a5359f0c4e9699e
junaidapdev <junaidap.dev@gmail.com>
2022-12-22 16:29:39 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Explore from './pages/Explore'
 import ForgotPassword from './pages/ForgotPassword'
 import Offers from './pages/Offers'
@@ -10,8 +11,21 @@ import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Explore/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      </Routes>
+        {/* Navbar */}
+        <Navbar/>
+    </Router>
+    </>
+    )
 }
 
 export default App
