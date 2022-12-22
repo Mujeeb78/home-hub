cee16827d937f14428d8b4d7e91181e1d5b52f09
junaidapdev <junaidap.dev@gmail.com>
2022-12-22 15:21:34 +0530

    pages added

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFireStore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDHykiV9PZ1VIlVZpAed3ASVca4GsGlzo4",
+  authDomain: "home-hub-14f7e.firebaseapp.com",
+  projectId: "home-hub-14f7e",
+  storageBucket: "home-hub-14f7e.appspot.com",
+  messagingSenderId: "668438977631",
+  appId: "1:668438977631:web:f2bd7e4e121a2fefde0fea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore()
