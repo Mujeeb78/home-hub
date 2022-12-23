404c52344fce9b1da4ea89b99747ff9b119bd7b7
junaidapdev <junaidap.dev@gmail.com>
2022-12-23 21:51:52 +0530

    user sign-in funtionality added

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { getAuth } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
+
 
 const Profile = () => {
-  return (
-    <div>Proxsxsfile</div>
-  )
+
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, [])
+
+  return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
 }
 
 export default Profile
