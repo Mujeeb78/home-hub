dea14c98f09e932bff13e3c3aa8b02537ecc67b3
junaidapdev <junaidap.dev@gmail.com>
2022-12-27 14:55:20 +0530

    Private route Component & useAuthStatus hook added

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar'
-import Explore from './pages/Explore'
-import ForgotPassword from './pages/ForgotPassword'
-import Offers from './pages/Offers'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Explore/>}/>
-        <Route path='/offers' element={<Offers/>}/>
-        <Route path='/sign-in' element={<SignIn/>}/>
-        <Route path='/sign-up' element={<SignUp/>}/>
-        <Route path='/profile' element={<Profile/>}/>      
-        <Route path='/forgot-password' element={<ForgotPassword/>}/>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path='/profile' element={<PrivateRoute />}>
+            <Route path='/profile' element={<Profile />} />
+          </Route>
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
         {/* Navbar */}
-        <Navbar/>
-    </Router>
+        <Navbar />
+      </Router>
 
-    <ToastContainer/>
+      <ToastContainer />
     </>
-    )
-}
+  );
+};
 
-export default App
+export default App;
